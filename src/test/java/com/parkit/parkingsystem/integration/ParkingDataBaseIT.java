e89c08fb295647d8bee1fe5f63f852e732f29e9e
Fabien De deken <fabien.dedeken@yahoo.fr>
2021-10-05 15:55:57 +0200

    writes testParkinACar()

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -13,7 +14,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -48,11 +55,62 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingACar(){
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-    }
+	public void testParkingACar() {
+		// GIVEN
+		Connection ticketConnection = null;
+		Connection parkingConnection = null;
+		ResultSet rsTicketSaved = null;
+		String resultParkingAvailable = "";
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		parkingService.processIncomingVehicle();
+
+		// WHEN
+		// retrieves the saved ticket in the database
+		try {
+			ticketConnection = ticketDAO.dataBaseConfig.getConnection();
+			PreparedStatement ps = ticketConnection.prepareStatement(DBConstants.GET_TICKET);
+			ps.setString(1, inputReaderUtil.readVehicleRegistrationNumber());
+			rsTicketSaved = ps.executeQuery();
+			ticketDAO.dataBaseConfig.closePreparedStatement(ps);
+			ticketDAO.dataBaseConfig.closeResultSet(rsTicketSaved);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ticketDAO.dataBaseConfig.closeConnection(ticketConnection);
+		}
+
+		// retrieves the parking spot in the ticket and searches it in the parking database
+		try {
+			ticketConnection = ticketDAO.dataBaseConfig.getConnection();
+			PreparedStatement ps = ticketConnection
+					.prepareStatement("select PARKING_NUMBER from ticket where VEHICLE_REG_NUMBER = ?");
+			ps.setString(1, inputReaderUtil.readVehicleRegistrationNumber());
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			parkingConnection = parkingSpotDAO.dataBaseConfig.getConnection();
+			ps = parkingConnection.prepareStatement("select AVAILABLE from parking where PARKING_NUMBER = ?");
+			ps.setString(1, rs.getString(1));
+			ResultSet rsParkingAvailable = ps.executeQuery();
+			rsParkingAvailable.next();
+			resultParkingAvailable = rsParkingAvailable.getString(1);
+
+			parkingSpotDAO.dataBaseConfig.closePreparedStatement(ps);
+			parkingSpotDAO.dataBaseConfig.closeResultSet(rsParkingAvailable);
+
+			ticketDAO.dataBaseConfig.closePreparedStatement(ps);
+			ticketDAO.dataBaseConfig.closeResultSet(rs);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ticketDAO.dataBaseConfig.closeConnection(ticketConnection);
+			parkingSpotDAO.dataBaseConfig.closeConnection(parkingConnection);
+		}
+
+		// THEN
+		assertThat(rsTicketSaved).isNotEqualTo(null);
+		assertThat(resultParkingAvailable).isEqualTo("0");
+	}
 
     @Test
     public void testParkingLotExit(){
